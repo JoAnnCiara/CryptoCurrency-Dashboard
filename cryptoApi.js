f587162e95ec50dd4d0ed66ed440335f3176f7b7
JoAnn Ciaramello <joannciaramello@gmail.com>
2022-03-21 16:07:15 -0400

    Update cryptoApi.js
    removed key

--- cryptoApi.js
+++ cryptoApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': 'b681743823msh2bde7fe1df3bb45p1d74a9jsna22f03b44de3'
+    'x-rapidapi-key': 'XXXXXXXXXXX'
  };
 
  const baseUrl = 'https://coinranking1.p.rapidapi.com';
